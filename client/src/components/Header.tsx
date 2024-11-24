fe5b8c9ea75ffb3279e0cbb786a15c430bede5d5
Josh Guarino <jguarino722@gmail.com>
2024-11-24 14:27:02 -0500

    more client work

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="bg-red-400 text-white w-full h-full border-2 rounded-lg">
+    <header className="bg-blue-400 text-white w-full h-full border-2 rounded-lg">
       <div className="mx-auto flex items-center justify-between px-4">
         <h1 className="text-2xl font-bold">Localize Chat Room</h1>
       </div>
